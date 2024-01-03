588faf17375097ef4f493ebd1fb6113499e0046a
m4fz <leva.320.chekmenev@gmail.com>
2024-01-04 03:08:16 +0700

    1 урок

--- src/main/java/ru/java_lessons/lesson1/OOP/Game.java
+++ src/main/java/ru/java_lessons/lesson1/OOP/Game.java
@@ -6,25 +6,52 @@ public class Game {
         GameUnit unit2 = new GameUnit();
         GameUnit unit3 = new GameUnit();
 
-        unit1.name = "ork";
-        unit1.speed = 50.5f;
-        unit1.weapon = "axe";
-        unit1.life = 2000;
+        System.out.println("\r\n" + "Unit 1:");
+        unit1.type = "Ork";
+        unit1.isHuman = false;
+        unit1.chClass = "Berserk";
+        unit1.setWeapon("Axe", "Spear");
+        unit1.speed = 3.8f;
+        unit1.hp = 175;
+        System.out.println("Type - " + unit1.type + "\r\n" + "Class - " + unit1.chClass + "\r\n" + unit1.setHumanity() + "\r\n");
+        System.out.println("Stats:" + "\r\n" + "Base HP = " + unit1.hp + "\r\n" + "Base speed = " + unit1.speed + "\r\n" + "Base weapons = " + unit1.weapons + "\r\n");
         unit1.run();
-        unit1.attack("Spear", 300);
-        unit1.cure(100, 3);
-        unit1.makeDamage();
+        unit1.attack();
+        unit1.takeDamage();
+        System.out.println(unit1.type + " now has " + unit1.hp + "hp.");
+        unit1.cure/* types: small,medium,large */("small",1);
+        unit1.speak();
 
-        System.out.println(unit1.name + " did " + unit1.damage + " damage");
-        System.out.println(unit1.life);
-        System.out.println(unit1.isHuman);
+        System.out.println("\r\n" + "Unit 2:");
+        unit2.type = "Dark Elf";
+        unit2.isHuman = false;
+        unit2.chClass = "Archer";
+        unit2.setWeapon("Bow", "Dagger");
+        unit2.speed = 8.1f;
+        unit2.hp = 75;
+        System.out.println("Type - " + unit2.type + "\r\n" + "Class - " + unit2.chClass + "\r\n" + unit2.setHumanity() + "\r\n");
+        System.out.println("Stats:" + "\r\n" + "Base HP = " + unit2.hp + "\r\n" + "Base speed = " + unit2.speed + "\r\n" + "Base weapons = " + unit2.weapons + "\r\n");
+        unit2.run();
+        unit2.attack();
+        unit2.takeDamage();
+        System.out.println(unit2.type + " now has " + unit2.hp + "hp.");
+        unit2.cure/* types: small,medium,large */("large",1);
+        unit2.speak();
 
-        unit2.name = "elf";
-        unit2.weapon = "bow";
-        unit2.isHuman = true;
-        unit2.cure(50, 2);
-        System.out.println(unit2.life);
-
-
+        System.out.println("\r\n" + "Unit 3:");
+        unit3.type = "Human";
+        unit3.isHuman = true;
+        unit3.chClass = "Knight";
+        unit3.setWeapon("Sword", "Shield");
+        unit3.speed = 5.3f;
+        unit3.hp = 125;
+        System.out.println("Type - " + unit3.type + "\r\n" + "Class - " + unit3.chClass + "\r\n" + unit3.setHumanity() + "\r\n");
+        System.out.println("Stats:" + "\r\n" + "Base HP = " + unit3.hp + "\r\n" + "Base speed = " + unit3.speed + "\r\n" + "Base weapons = " + unit2.weapons + "\r\n");
+        unit3.run();
+        unit3.attack();
+        unit3.takeDamage();
+        System.out.println(unit3.type + " now has " + unit3.hp + "hp.");
+        unit3.cure/* types: small,medium,large */("medium",1);
+        unit3.speak();
     }
 }
--- src/main/java/ru/java_lessons/lesson1/OOP/GameUnit.java
+++ src/main/java/ru/java_lessons/lesson1/OOP/GameUnit.java
@@ -1,35 +1,112 @@
 package ru.java_lessons.lesson1.OOP;
 
-import com.sun.security.jgss.GSSUtil;
+import java.lang.ref.Reference;
 
 public class GameUnit {
-    String name;
-    String weapon;
+    String type;
+    String humanity;
+    String chClass;
+    String weapons;
+    String primaryWeapon;
+    String secondaryWeapon;
+    String attackWeapon;
+    int healingCapacity;
+    int healAmount;
     int damage;
     float speed;
-    int life;
+    int hp;
+    int maxHp;
     boolean isHuman;
+    boolean weaponType;
 
-
+    void setMaxHp(){
+        maxHp = hp;
+    }
     void run() {
-        System.out.println(name + " is running");
+        System.out.println(type + " is approaching an enemy with a speed of " + speed + " km/h." + "\r\n");
     }
-
-    int attack(String weapon, int damage) {
-        System.out.println(name + " did " + damage + " damage" + " with " + weapon);
-        return  damage;
+    void setWeapon(String weapon1, String weapon2){
+        primaryWeapon = weapon1;
+        secondaryWeapon = weapon2;
+        weapons = primaryWeapon + ", " + secondaryWeapon;
     }
-    void cure(int bottle, int count) {
-        System.out.println(name + " is curing");
-        life += (bottle * count);
-    }
-
-    void makeDamage(){
-        int defRandom = 300;
-        double randomNum = Math.random();
-        if(randomNum >= 0.2){
-            damage = (int) (randomNum * 1000);
+    void rollForDamage(){
+        int randomNum = (int) (Math.random() * 101);
+        if(randomNum >= 30){
+            damage = randomNum;
+            return;
         }
-        damage = defRandom;
+        damage = 0;
+    }
+    void setAttackWeapon(){
+        double randomWeapon = Math.random();
+        if (randomWeapon >= 0.5){
+            weaponType = true;
+            return;
+        }
+        weaponType = false;
+    }
+    void attack() {
+        setAttackWeapon();
+        if (weaponType == true){
+            attackWeapon = primaryWeapon;
+        }
+        else {attackWeapon = secondaryWeapon;
+        }
+        rollForDamage();
+        if(damage == 0){
+            System.out.println(type + " failed his attack." + "\r\n");
+        }
+        else if(attackWeapon.equalsIgnoreCase("Shield")){
+            System.out.println("This turn " + type + " did " + damage + " damage" + " with a " + attackWeapon + "bash" + "\r\n");
+        }
+        else {
+            System.out.println("This turn " + type + " did " + damage + " damage" + " with a " + attackWeapon + "\r\n");
+        }
+    }
+    void takeDamage() {
+        setMaxHp();
+        rollForDamage();
+        if(damage == 0){
+            System.out.println(type + " didn't take any damage.");
+        }
+        else {System.out.println("This turn " + type + " took " + damage + " damage");
+            hp = hp - damage;
+            if (hp <= 0){
+                System.out.println(type + " took too much damage and used an ability " + "\033[3m'Last Chance'\033[0m" + " to escape death.");
+                hp = 5;
+            }
+        }
+    }
+    String setHumanity(){
+        if(!isHuman) {
+            return humanity = "Character is not human.";
+        }
+        return humanity = "Character is human.";
+    }
+    void cure(String bottleType, int times) {
+        if (hp < maxHp){
+            if(bottleType.equalsIgnoreCase("small")){
+                healingCapacity = 25;
+            }
+            if (bottleType.equalsIgnoreCase("medium")){
+                healingCapacity = 50;
+            }
+            if (bottleType.equalsIgnoreCase("large")){
+                healingCapacity = 100;
+            }
+            healAmount = healingCapacity * times;
+            hp = hp + healAmount;
+            if (hp > maxHp){
+                hp = maxHp;
+            }
+            System.out.println(type + " is curing for " + healAmount + " hp." + "\r\n" + "Total hp - " + hp + "." + "\r\n");
+        }
+        else{
+            System.out.println(type + " does not need healing." + "\r\n");
+        }
+    }
+    void speak(){
+        System.out.println(type + " is now speaking with a merchant." + "\r\n");
     }
 }
