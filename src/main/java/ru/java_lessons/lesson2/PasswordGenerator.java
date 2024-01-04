1e3265d89296362a3b42d01fe57d143ad96a702c
m4fz <leva.320.chekmenev@gmail.com>
2024-01-04 21:58:08 +0700

    Урок 2

--- src/main/java/ru/java_lessons/lesson2/PasswordGenerator.java
+++ src/main/java/ru/java_lessons/lesson2/PasswordGenerator.java
@@ -1,52 +1,118 @@
 package ru.java_lessons.lesson2;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class PasswordGenerator {
-    final static int ASCII_START_NUMBERS = 48;
-    final static int ASCII_END_NUMBERS = 57;
+    final static int ASCII_NUMBERS_START = 48;
+    final static int ASCII_NUMBERS_END = 57;
     final static int ASCII_BIG_LETTER_START = 65;
     final static int ASCII_BIG_LETTER_END = 90;
     final static int ASCII_SMALL_LETTER_START = 97;
     final static int ASCII_SMALL_LETTER_END = 122;
+    static String onlyD = "";
+    static String onlyL = "";
+    static int psLength = 0;
+
 
     public static void main(String[] args) {
-        System.out.println(generatePassword(8, false, true));
-    }
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Start password generator? (to start type - \033[3m'y'\033[0m)");
+        String go = scan.next();
+        int passNum = 1;
+        while(!go.equalsIgnoreCase("y")){
+            System.out.println("\u001B[31m" + "\033[3m'invalid command'\033[0m");
+            go = scan.next();
+        }
+        while (!go.equalsIgnoreCase("q") & go.equalsIgnoreCase("y")){
+            System.out.println("Set password length and choose if it will be generated form only numbers or letters");
+            System.out.println("Password length:");
+            psLength = scan.nextInt();
+            while (!onlyD.equalsIgnoreCase("y") | !onlyD.equalsIgnoreCase("n")) {
+                System.out.println("Only digits?(y/n) -");
+                onlyD = scan.next();
+                if(onlyD.equalsIgnoreCase("y")){
+                    break;
+                } else if (onlyD.equalsIgnoreCase("n")) {
+                    while (!onlyL.equalsIgnoreCase("y") | !onlyL.equalsIgnoreCase("n")) {
+                        System.out.println("Only letters?(y/n) -");
+                        onlyL = scan.next();
+                        if(onlyL.equalsIgnoreCase("y") | onlyL.equalsIgnoreCase("n")){
+                            break;
+                        }
+                    }
+                }
+                break;
+            }
+            System.out.println(generatePassword(psLength));
+            System.out.println("Run again?(to run,type - y, to quit,type - q)");
+            go = scan.next();
+            if(go.equalsIgnoreCase("y")){
+                passNum++;
+                System.out.println("Password number " + passNum + ":");
+            } else if (go.equalsIgnoreCase("q")) {
+                System.out.println("\u001B[36m" + "\033[3mGoodbye!\033[0m");
+                break;
+            } else {
+                while(!go.equalsIgnoreCase("y")){
+                    System.out.println("\u001B[31m" + "\033[3m'invalid command'\033[0m");
+                    System.out.println("Run again?(to run,type - y, to quit,type - q)");
+                    go = scan.next();
+                    if(go.equalsIgnoreCase("q")){
+                        System.out.println("\u001B[36m" + "\033[3mGoodbye!\033[0m");
+                        break;
+                    }
+                    if(go.equalsIgnoreCase("y")){
+                        passNum++;
+                        System.out.println("Password number " + passNum + ":");
+                    }
+                }
+            }
 
-    /**
-     *
-     * @param passwordLength
-     * @param onlyDigits
-     * @param onlyLetters
-     * @return
+        }
+
+
+    }
+    /** Выбор метода генерации пароля
+     * @param passwordLength Длинна пароля
+     * onlyD Параметр генерирования только чисел
+     * onlyL Параметр генерирования только букв
+     * @return Возврат метода генерации пароля
      */
-    static String generatePassword(int passwordLength, boolean onlyDigits, boolean onlyLetters) {
+    static String generatePassword(int passwordLength) {
         // Numbers from 48 to 57
-        // Letters 65 to 90 , 97 to 122
+        // Letters, big 65 to 90 , small 97 to 122
         String password = "";
-        if (onlyDigits){
-            password = generateOnlyStringPassword(passwordLength);
-        } else if (onlyLetters) {
-            password = generateOnlyNumbersPassword(passwordLength);
+        if (onlyL.equals("y")){
+            password = generateOnlyLetters(passwordLength);
+        } else if (onlyD.equals("y")) {
+            password = generateOnlyNumbers(passwordLength);
         } else {
-            password = generatePassword(passwordLength);
+            password = generatePasswordAll(passwordLength);
         }
         return password;
     }
-
-    static String generateOnlyNumbersPassword(int passwordLength) {
+    /**
+     * Модуль генерации пароля используя только числа
+     * @param passwordLength Запись случайных чисел на всю заданную длину пароля
+     * @return Возврат конечного пароля
+     */
+    static String generateOnlyNumbers(int passwordLength) {
         String password = "";
         Random random = new Random();
         int i = 0;
         while (i < passwordLength) {
             i++;
-            password += (char) random.nextInt(ASCII_START_NUMBERS, ASCII_END_NUMBERS + 1);
+            password += (char) random.nextInt(ASCII_NUMBERS_START, ASCII_NUMBERS_END + 1);
         }
         return password;
     }
-
-    static String generateOnlyStringPassword(int passwordLength) {
+    /**
+     * Модуль генерации пароля используя только буквы
+     * @param passwordLength Запись случайных букв на всю заданную длину пароля
+     * @return Возврат конечного пароля
+     */
+    static String generateOnlyLetters(int passwordLength) {
         int k = 0;
         String password = "";
         Random random = new Random();
@@ -60,8 +126,12 @@ public class PasswordGenerator {
         }
         return password;
     }
-
-    static String generatePassword(int passwordLength){
+    /**
+     * Модуль генерации пароля используя и числа и буквы
+     * @param passwordLength Запись случайных чисел и букв на всю заданную длину пароля
+     * @return Возврат конечного пароля
+     */
+    static String generatePasswordAll(int passwordLength){
         int j = 0;
         String password = "";
         Random random = new Random();
@@ -73,7 +143,7 @@ public class PasswordGenerator {
             } else if (choice == 1) {
                 password += (char) random.nextInt(ASCII_SMALL_LETTER_START, ASCII_SMALL_LETTER_END + 1);
             } else {
-                password += (char) random.nextInt(ASCII_START_NUMBERS, ASCII_END_NUMBERS + 1);
+                password += (char) random.nextInt(ASCII_NUMBERS_START, ASCII_NUMBERS_END + 1);
             }
         }
         return password;
