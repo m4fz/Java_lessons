491fbb85e7a5788b39f2255a181d49e1b608ab34
m4fz <leva.320.chekmenev@gmail.com>
2024-01-07 16:32:15 +0700

    Урок 4 - access modifiers

---
+++ src/main/java/ru/java_lessons/lesson4/modifiers/ModTest.java
@@ -0,0 +1,13 @@
+package ru.java_lessons.lesson4.modifiers;
+
+import ru.java_lessons.lesson4.modifiers.Mod1;
+
+public class ModTest {
+    public static void main(String[] args) {
+        Mod1 modifier1 = new Mod1();
+
+        modifier1.a = 100;
+
+    }
+
+}
